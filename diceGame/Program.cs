cb964e40150546c11eb9357dd2691d7d7ca99d00
eecmanny <130661353+eecmanny@users.noreply.github.com>
2024-10-08 16:16:04 -0400

    Update Program.cs

--- diceGame/Program.cs
+++ diceGame/Program.cs
@@ -2,72 +2,90 @@ bool appOff = false;
 // Instantiate the DicePick class
 DicePick userDicePick = new DicePick();
 // Instantiate the Random number class
+DiceRollGenerator roll = new DiceRollGenerator();
+////verified random roll method works
+//Console.WriteLine(roll.DiceRoll());
+
+Console.WriteLine(roll.dice);
 
 
 Console.WriteLine("Do you want to play a game?");
 Console.WriteLine("");
 Console.WriteLine("Dice Just Rolled");
 Console.WriteLine("Please choose a number 1 through 6");
-string dicePick = Console.ReadLine();
+string userTypedDicePick = Console.ReadLine();
+
+int userTypedDicePickNumber = int.Parse(userTypedDicePick); 
 
 
 
 
 
 
-switch (dicePick)
+
+switch (userTypedDicePick)
 {
     case "1":
         userDicePick.UserPick();
         break;
     case "2":
         userDicePick.UserPick();
-        break;    
+        break;
     case "3":
         userDicePick.UserPick();
-        break;    
+        break;
     case "4":
         userDicePick.UserPick();
-        break;    
+        break;
     case "5":
         userDicePick.UserPick();
-        break;    
+        break;
     case "6":
         userDicePick.UserPick();
         break;
     default:
         Console.WriteLine("Invalid option. Please choose 1, 2, 3, 4, 5, or 6.");
-    break;
+        break;
 }
 
 
 
 
 
-class DiceRollGenerator
+public class DiceRollGenerator
 {
-    public DiceRollGenerator()
-    {
-        
-    }
-
     private Random randomRoll = new Random();
-    public void DiceRoll()
+    public int DiceRoll()
     {
-        //int month = rnd.Next(1, 13);  // creates a number between 1 and 12
-        //int dice = rnd.Next(1, 7);   // creates a number between 1 and 6
-        //int card = rnd.Next(52);     // creates a number between 0 and 51
         int dice = randomRoll.Next(1, 7);   // creates a number between 1 and 6
+
+        Console.WriteLine(dice);
+        return dice;
     }
-}
 }
 
 public class DicePick
 {
-    private int userTurnes = 0;
+    private int userTurn = 0;
 
     public void UserPick()
     {
+        if (userTurn == 0)
+        {
+            userTurn++;
+            if (userTypedDicePickNumber == roll.DiceRoll()) 
+            {
+                Console.WriteLine("Number doesn't match try again");
+            }
+            else if(userTurn == 1)
+            {
+                Console.WriteLine("Sorry you guessed wrong again, you have one more chance.");
+            }
+            else 
+            {
+                Console.WriteLine("You lose!");
+
+        }
         Console.WriteLine("method works");
     }
 }
@@ -75,5 +93,8 @@ public class DicePick
 
 class DiceMatchUp
 {
-
+    private void DiceNumberGuess()
+    {
+        Console.WriteLine("method worked");
+    }
 }
